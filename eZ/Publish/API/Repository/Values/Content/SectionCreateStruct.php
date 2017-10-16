75ab60f9567cf636e264df1dc7ee8204ea32d758
Ayoub EL ABOUSSI <elaboussi.ayoub@gmail.com>
2017-10-16 12:41:34 +0100

    errors fix

---
+++ eZ/Publish/API/Repository/Values/Content/SectionCreateStruct.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * File containing the eZ\Publish\API\Repository\Values\Content\SectionCreateStruct class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\API\Repository\Values\Content;
+
+/**
+ * This class represents a section
+ */
+class SectionCreateStruct extends SectionStruct
+{
+}
